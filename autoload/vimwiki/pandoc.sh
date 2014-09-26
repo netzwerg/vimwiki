b849f44abcfa497cf1c1ed4286ecb0d373a45ca6
Rahel Lüthy <rahel.luethy@gmail.com>
2014-09-26 12:24:24 +0200

    Actually replace links (simpler than before, but works now)

--- autoload/vimwiki/pandoc.sh
+++ autoload/vimwiki/pandoc.sh
@@ -21,19 +21,9 @@ OUTPUTDIR="$4"
 INPUT="$5"
 CSSFILE="$6"
 
-FORCEFLAG=
-
-[ $FORCE -eq 0 ] || { FORCEFLAG="-f"; };
-
 OUTPUT="$OUTPUTDIR"/$(basename "$INPUT" .$EXTENSION).html
 
-VALID_CHARS='[^][\\]'  # ']' must be the first character in the '[...]' or '[^...]
-PROTOCOLS='\(https:\/\/\|http:\/\/\|www.\|ftp:\/\/\|file:\/\/\|mailto:\)'
-SED_CMD1="s/\[\($VALID_CHARS\+\)\](\($VALID_CHARS\+\))/[\1](\2.html)/g"
-SED_CMD2="s/\[\($VALID_CHARS\+\)\](\($PROTOCOLS\($VALID_CHARS\)\+\).html)/[\1](\2)/g"
-SED_CMD3="s/\[\($VALID_CHARS\+\)\]()/[\1](\1.html)/g"
-
 cat "$INPUT" | \
-    sed "$SED_CMD1; $SED_CMD2; $SED_CMD3;" |
+  sed "s/\[\[\(.*\)\]\]/\[\1\]\(\1.html\)/g" | \
     "$PANDOC" -s --template bootstrap.html -f markdown -c "$CSSFILE" -o "$OUTPUT"
 cp ~/.vim/bundle/vimwiki/autoload/vimwiki/style.css $OUTPUTDIR
